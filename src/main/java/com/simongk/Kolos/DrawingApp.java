d89f419043d3b8da69e7127fad2677d1edc26a53
simongk <simongk95@gmail.com>
2017-04-25 12:36:06 +0200

    second commit

--- src/main/java/com/simongk/Kolos/DrawingApp.java
+++ src/main/java/com/simongk/Kolos/DrawingApp.java
@@ -11,4 +11,8 @@ public class DrawingApp {
     public int multiply(int a, int b) {
         return a*b;
     }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
 }
--- src/test/java/com/simongk/Kolos/DrawingAppTest.java
+++ src/test/java/com/simongk/Kolos/DrawingAppTest.java
@@ -37,6 +37,14 @@ public class DrawingAppTest {
         Assert.assertEquals(12,result);
     }
 
+    @Test
+    public void shouldDivideTwoNumbers(){
+        int a=10; int b=5;
 
+        int result = drawingApp.divide(a,b);
+
+        Assert.assertEquals(2, result);
+
+    }
 
 }
